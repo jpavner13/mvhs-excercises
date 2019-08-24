27df6d861025fc57101d24e5dff320e2ce415435
Pierce Avner <jpavner@gmail.com>
2019-08-23 20:52:56 -0600

    First two Chapter11 Excercises

---
+++ Chapter11/objectString.java
@@ -0,0 +1,12 @@
+public class objectString {
+
+    public static void main(String[] args){
+        String str = "Hello World";
+        String secondVariable ;
+
+        secondVariable = new String( "Whats up my dude" );
+
+        System.out.println( str );
+        System.out.println( secondVariable );
+    }
+}
---
+++ Chapter11/stringLength.java
@@ -0,0 +1,10 @@
+public class stringLength {
+    public static void main(String[] args){
+        String str ;
+        int len;
+
+        str = new String ( "Elementary my dear Watson!" );
+        len = str.length ();
+        System.out.println("The length is: " + len);
+    }
+}
