c45bb04ed62398657798831cf4cccb67be5123ed
Pierce Avner <jpavner@gmail.com>
2019-10-07 12:53:03 -0600

    worked on PigLikeGame

--- Chapter30/PigLikeGame.java
+++ Chapter30/PigLikeGame.java
@@ -11,16 +11,23 @@ public class PigLikeGame{
         boolean playing = true ;
 
         int previousRoll = 10 ;
+        int roundScore = 0 ;
 
         while(playing){
-            int roundScore = 0 ;
             String rollAgain = "" ;
 
-            System.out.println(previousRoll);
-
             if (previousRoll <= 1 ){
-                playing = false ;
-                System.out.println("End");
+                roundScore = 0;
+                int x = 0;
+                    int diceroll = 0 ;
+                    while (x <= 3){
+                        diceroll = rand.nextInt(6) + diceroll;
+                        x++ ;
+                    }
+                    compScore = compScore + diceroll ;
+                    System.out.println("The computer rolled a total of: " + diceroll);
+                    System.out.println("");
+                    previousRoll = 10 ;
             } else {
                 System.out.println("Roll Again? y --> yes    n --> no.");
                 rollAgain = scan.nextLine();
@@ -28,22 +35,38 @@ public class PigLikeGame{
                 if (rollAgain.equals("y")){
                     int roll = rand.nextInt(6) + 1 ;
                     System.out.println("Your roll is: " + roll);
+                    System.out.println("");
                     roundScore = roundScore + roll ;
                     previousRoll = roll ;
-            } else {
+                    roundScore = roundScore + roll ;
+                } else {
                     userScore = userScore + roundScore ;
-                    playing = false ;
+                    roundScore = 0;
+                    int x = 0;
+                    int diceroll = 0 ;
+                    while (x <= 3){
+                        diceroll = rand.nextInt(6) + diceroll;
+                        x++ ;
+                    }
+                    compScore = compScore + diceroll ;
+                    System.out.println("The computer rolled a total of: " + diceroll);
+                    System.out.println("");
                 }
 
             }
 
-            if (playing){
-                userScore = roundScore + userScore ;
-            } else {
-                userScore = userScore - roundScore;
+            if (userScore >= 100){
+                System.out.println("User wins! Their score is: " + userScore);
+                System.out.println("Computer score is: " + compScore);
+                playing = false;
+            }
+
+            if (compScore >= 100){
+                System.out.println("Computer wins! Their score is: " + compScore);
+                System.out.println("User score is: " + userScore);
+                playing = false;
             }
         }
         
-        System.out.println(userScore);
     }
 }
